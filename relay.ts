e03f01402ac4cbadf5693ce007a9a8cbebf3ba2f
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-13 14:57:32 +0200

    Aktualisiere main.blocks, main.ts, relay.ts

--- relay.ts
+++ relay.ts
@@ -1,1 +1,52 @@
-// Gib deinen Code hier ein
+
+//% color=#7F0000 icon="\uf011" block="Relay" weight=05
+namespace relay
+/* 231013 https://github.com/calliope-net/relay
+
+Code anhand der Python- und  Arduino Library neu programmiert von Lutz Elßner im Oktober 2023
+*/ {
+    export enum eADDR { Relay_x18 = 0x18, Relay_x19 = 0x19 }
+    let n_i2cCheck: boolean = false // i2c-Check
+    let n_i2cError: number = 0 // Fehlercode vom letzten WriteBuffer (0 ist kein Fehler)
+
+
+    //% group="beim Start"
+    //% block="i2c %pADDR i2c-Check %ck" weight=4
+    //% pADDR.shadow="relay_eADDR"
+    //% ck.shadow="toggleOnOff" ck.defl=1
+    export function beimStart(pADDR: number, ck: boolean) {
+        n_i2cCheck = (ck ? true : false) // optionaler boolean Parameter kann undefined sein
+        n_i2cError = 0 // Reset Fehlercode
+    }
+
+
+
+
+    // ========== group="i2c Adressen"
+
+    //% blockId=relay_eADDR
+    //% group="i2c Adressen" advanced=true
+    //% block="%pADDR" weight=4
+    export function relay_eADDR(pADDR: eADDR): number { return pADDR }
+
+    //% group="i2c Adressen" advanced=true
+    //% block="i2c Fehlercode" weight=2
+    export function i2cError() { return n_i2cError }
+
+    function i2cWriteBuffer(pADDR: number, buf: Buffer, repeat: boolean = false) {
+        if (n_i2cError == 0) { // vorher kein Fehler
+            n_i2cError = pins.i2cWriteBuffer(pADDR, buf, repeat)
+            if (n_i2cCheck && n_i2cError != 0)  // vorher kein Fehler, wenn (n_i2cCheck=true): beim 1. Fehler anzeigen
+                basic.showString(Buffer.fromArray([pADDR]).toHex()) // zeige fehlerhafte i2c-Adresse als HEX
+        } else if (!n_i2cCheck)  // vorher Fehler, aber ignorieren (n_i2cCheck=false): i2c weiter versuchen
+            n_i2cError = pins.i2cWriteBuffer(pADDR, buf, repeat)
+        //else { } // n_i2cCheck=true und n_i2cError != 0: weitere i2c Aufrufe blockieren
+    }
+
+    function i2cReadBuffer(pADDR: number, size: number, repeat: boolean = false): Buffer {
+        if (!n_i2cCheck || n_i2cError == 0)
+            return pins.i2cReadBuffer(pADDR, size, repeat)
+        else
+            return Buffer.create(size)
+    }
+} // relay.ts
